ef326460c304fe0f630f4db94fa2c3c0a3855e21
andreassbayu <andreass.bayu@gmail.com>
2016-03-23 17:59:40 +0700

    firest commit

---
+++ config/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+	list menu yang akan dipanggil oleh APP 
+	hanya class yang ada di routes dibawah ini yang dijalankan oleh aplikasi
+*/
+
+$ROUTES = array(
+		'home',
+                'statistik',
+                'profil',
+                'tatacara',
+                'pengaduan',
+		'register',
+                'recovery',
+                'login',
+                'user'
+        );
+
+$ROUTES_IGNORE = array(
+                'home',
+                'statistik',
+                'tatacara',
+                'register',
+                'recovery',
+                'login',
+        );
+?>
+
