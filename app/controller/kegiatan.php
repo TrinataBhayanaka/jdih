bd68d044560f8c9be23146316f3e6fc36bd00ed5
ilmawan <ilmawan.muharam@gmail.com>
2016-06-24 05:59:24 +0700

    update iman

--- app/controller/kegiatan.php
+++ app/controller/kegiatan.php
@@ -74,12 +74,23 @@ class kegiatan extends Controller {
 		$this->view->assign('count_berita',$count_berita);
 		
 		//end sidebar
-		/*$item_per_page = 3;
-		$data = $this->contentHelper->ajax_count();
-		$pages = ceil($data['hit']/$item_per_page);
-		$this->view->assign('pages',$pages);*/
 		//content
-		$berita2 = $this->contentHelper->GetData('jdih_berita',1,'n_status = 1 and publish = 1 and jenis = 1 ','id_berita desc LIMIT 5');
+		// pr($_POST);
+		if($_POST['tanggal']){
+			$tmp_tgl_start =$_POST['tanggal'];
+			$ex_1 = explode('/',$tmp_tgl_start);
+			$tgl_start_fix = $ex_1['2'].'-'.$ex_1['1'].'-'.$ex_1['0']; 
+			
+			$ext = "and tanggal = '{$tgl_start_fix}'";
+			$param = $tgl_start_fix;
+			$where = "n_status = 1 and publish = 1 and jenis = 1 {$ext}";
+			$where_ext = "n_status = 1 and publish = 1 and jenis = 1 {$ext}";
+		}else{
+			$param = '';
+			$where = "n_status = 1 and publish = 1 and jenis = 1 ";
+			$where_ext = "n_status = 1 and publish = 1 and jenis = 1 ";
+		}
+		$berita2 = $this->contentHelper->GetData('jdih_berita',1,$where,'tanggal LIMIT 3');
 		if(berita2){
 			foreach ($berita2 as $keys2=> $value){
 				$tmp3[] = $value;
@@ -87,23 +98,54 @@ class kegiatan extends Controller {
 				$tmp3[$keys2]['hit'] = $hit_count_3['hit'];
 			}
 		}
+		
 		$this->view->assign('content',$tmp3);
+		//param ajax
+		$this->view->assign('param',$param);
 		$param_berita2 = 'berita2';
 		$count_berita2 = 'ref_berita2';
 		$this->view->assign('param_berita2',$param_berita2);
 		$this->view->assign('count_berita2',$count_berita2);
+		// exit;
+		//paging
+		$totalProduk = $this->contentHelper->countData('jdih_berita',$where_ext);
+		// pr($totalProduk);
+		$this->view->assign('totalProduk',$totalProduk);
 		return $this->loadView('berita/kegiatan');
     }
 	
-	/*function ajax_count(){
-		$item_per_page = 3;
-		$data = $this->contentHelper->ajax_count();
-		pr($data);
-		$pages = ceil($data['hit']/$item_per_page);
-		pr($pages);
-		// $newformat = array($pages);
-		print json_encode($pages);
-	}*/
+	function ajaxBeritaHukum()
+    {
+		global $basedomain;
+    	$num = $_GET['site'];
+    	$item_perpage = 3;
+    	$position = ($num-1) * $item_perpage;
+		if($_GET['condt']){
+			$ext = "and tanggal = '{$_GET['condt']}'";
+			$where = "n_status = 1 and publish = 1 and jenis = 1 {$ext}";
+		}else{
+			$where = "n_status = 1 and publish = 1 and jenis = 1";
+		}
+		$berita2 = $this->contentHelper->fetchData('jdih_berita',1,$where,'tanggal',"{$position},{$item_perpage}");
+
+    	if($berita2){
+			foreach ($berita2 as $keys2=> $value){
+				$tmp3[] = $value;
+				$hit_count_3 = $this->contentHelper->statistik($value['id_berita'],2);
+				$tmp3[$keys2]['hit'] = $hit_count_3['hit'];
+			}
+		}
+
+    	$this->view->assign('content',$tmp3);
+		$param_berita2 = 'berita2';
+		$count_berita2 = 'ref_berita2';
+		$this->view->assign('param_berita2',$param_berita2);
+		$this->view->assign('count_berita2',$count_berita2);
+    	$html = $this->loadView('modal/beritahukum');
+    	echo $html;
+    	exit;
+    }
+
 	
 	function ajax_select(){
 		$produk = $this->contentHelper->GetData('jdih_produk',1,'n_status = 1 and publish = 1 and posisi =1','id_produk desc LIMIT 3');
@@ -249,7 +291,21 @@ class kegiatan extends Controller {
 		$this->view->assign('param_berita',$param_berita);
 		$this->view->assign('count_berita',$count_berita);
 		
-		$berita2 = $this->contentHelper->GetData('jdih_berita',1,'n_status = 1 and publish = 1 and jenis = 2 ','id_berita desc LIMIT 5');
+		if($_POST['tanggal']){
+			$tmp_tgl_start =$_POST['tanggal'];
+			$ex_1 = explode('/',$tmp_tgl_start);
+			$tgl_start_fix = $ex_1['2'].'-'.$ex_1['1'].'-'.$ex_1['0']; 
+			
+			$ext = "and tanggal = '{$tgl_start_fix}'";
+			$param = $tgl_start_fix;
+			$where = "n_status = 1 and publish = 1 and jenis = 2 {$ext}";
+			$where_ext = "n_status = 1 and publish = 1 and jenis = 2 {$ext}";
+		}else{
+			$param = '';
+			$where = "n_status = 1 and publish = 1 and jenis = 2 ";
+			$where_ext ="n_status = 1 and publish = 1 and jenis = 2";
+		}
+		$berita2 = $this->contentHelper->GetData('jdih_berita',1,$where,'tanggal LIMIT 3');
 			if(berita2){
 				foreach ($berita2 as $keys2=> $value){
 					$tmp3[] = $value;
@@ -259,8 +315,52 @@ class kegiatan extends Controller {
 			}
 		// pr($tmp3);
 		$this->view->assign('content',$tmp3);
+		$this->view->assign('param',$param);
+		$totalProduk = $this->contentHelper->countData('jdih_berita',$where_ext);
+		$this->view->assign('totalProduk',$totalProduk);
 		return $this->loadView('berita/kajian2');
 	}
+	
+	function ajaxKajianHukum()
+    {
+		global $basedomain;
+    	$num = $_GET['site'];
+    	$item_perpage = 3;
+    	$position = ($num-1) * $item_perpage;
+		if($_GET['condt']){
+			$ext = "and tanggal = '{$_GET['condt']}'";
+			$where = "n_status = 1 and publish = 1 and jenis = 2 {$ext}";
+		}else{
+			$where = "n_status = 1 and publish = 1 and jenis = 2";
+		}
+		$berita2 = $this->contentHelper->fetchData('jdih_berita',1,$where,'tanggal',"{$position},{$item_perpage}");
+
+    	if($berita2){
+			foreach ($berita2 as $keys2=> $value){
+				$tmp3[] = $value;
+				$hit_count_3 = $this->contentHelper->statistik($value['id_berita'],2);
+				$tmp3[$keys2]['hit'] = $hit_count_3['hit'];
+			}
+		}
+
+    	$this->view->assign('content',$tmp3);
+		//param ajax
+		//produk
+		$param_produk = 'produk';
+		$count_produk = 'ref_produk';
+		$this->view->assign('param_produk',$param_produk);
+		$this->view->assign('count_produk',$count_produk);
+		
+		//berita
+		$param_berita = 'berita';
+		$count_berita = 'ref_berita';
+		$this->view->assign('param_berita',$param_berita);
+		$this->view->assign('count_berita',$count_berita);
+		
+    	$html = $this->loadView('modal/beritahukum');
+    	echo $html;
+    	exit;
+    }
 	
 	function details(){
 	//start sidebar
